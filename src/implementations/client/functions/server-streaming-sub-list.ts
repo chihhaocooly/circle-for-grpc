aa7c604f452fa5313322fe397ad5b75e076e2126
JeffAiii <jeff@aiii.ai>
2022-09-21 22:23:34 +0800

    留下教學需要用的程式碼

--- src/implementations/client/functions/server-streaming-sub-list.ts
+++
@@ -1,6 +0,0 @@
-export const ServerStreamingSubList = async (client: any) => {
-    console.log('Client: ServerStreamingSubList start');
-    client.ServerStreamingSubList({ name: '', price: 0, opStr: '' }).on('data', (data: any) => {
-        console.log('Client: ServerStreamingSubList => ', data);
-    });
-}
--- src/implementations/server/todoService.ts
+++ src/implementations/server/todoService.ts
@@ -1,22 +1,5 @@
 import { BidiCircleInfoData } from "./functions/bidiCircleInfoData";
-import { BidirectionalStreamingAsyncList } from "./functions/bidirectional-streaming";
-import { ClientStreamingAddItem } from "./functions/client-streaming-add-item";
-import { ClientStreamingAddLog } from "./functions/client-streaming-add-log";
-import { ServerStreamingSubList } from "./functions/server-streaming-sub-list";
-import { ServerStreamingSubMemberRecord } from "./functions/server-streaming-sub-member-record";
-import { UnaryAddItem } from "./functions/unary-add-item";
-import { UnaryAddMemberRecord } from "./functions/unary-add-member-record";
-
 
 export {
-    UnaryAddItem,
-    ClientStreamingAddItem,
-    ServerStreamingSubList,
-    BidirectionalStreamingAsyncList,
-
-    UnaryAddMemberRecord,
-    ServerStreamingSubMemberRecord,
-    ClientStreamingAddLog,
-
     BidiCircleInfoData,
 }
